5a4fd76567b9e19b78be7c7d35c1538035932b6d
urbanhobbit <33397688+urbanhobbit@users.noreply.github.com>
2025-11-18 04:46:52 +0300

    CIOGame04 için Pages build (docs)

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CIOGame03/",   // repo adı
+  base: "/CIOGame04/",   // 👈 repo adı
   build: {
-    outDir: "docs",      // Pages buradan servis edecek
+    outDir: "docs",      // 👈 Pages buradan servis edecek
   },
 });
