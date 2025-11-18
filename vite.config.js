c838f38a7967ad7f2bf086abacaeef1325fb613c
urbanhobbit <33397688+urbanhobbit@users.noreply.github.com>
2025-11-18 05:38:46 +0300

    Build and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CIOGame04/",   // 👈 repo adı
-  build: {
-    outDir: "docs",      // 👈 Pages buradan servis edecek
-  },
-});
+  base: '/CIOGame04/', // repo adın ne ise onu yaz
+})
